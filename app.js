2d28f4a60aee216ffb02ea46ce555096ef1772a9
seongEun <dsa020701@gmail.com>
2023-11-30 17:41:14 +0900

    깃 초기화

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// express 사용하지 않고 서버 띄우기
+// const http = require('http');
+// const app = http.createServer((req, res) => {
+
+//   res.writeHead(200 , { "content-Type" : "text/html; charset=utf-8" });
+
+//   console.log(req.url);
+//   if(req.url === '/'){
+//     res.end("루트이다.");
+//   }
+
+//   if(req.url === '/login'){
+//     res.end("로그인 화면");
+//   }
+// });
+
+// app.get('/', (req, res) => {
+//   res.send("wow")
+// })
+
+// app.listen(3001, () => {
+//   console.log('listening on port 3001');
+// })
+
+const express = require('express'); // 모듈
+const app = express();
+const home = require("./routes/home"); // 라우팅
+
+
+
+// 앱 세팅
+app.set("views", "./views");
+app.set('view engine', 'ejs');
+
+
+app.use("/", home); // 미들웨어 use 미들웨어를 등록해주는 메서드
+
+module.exports = app;
