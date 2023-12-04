30c8b9c6a73dd94fd77132fd0f0d8d84ccd8d4ba
seongEun <dsa020701@gmail.com>
2023-12-04 13:46:08 +0900

    로그인 프론트 작업

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log("ddd")
+const id = document.querySelector("#id"), 
+psword = document.querySelector("#psword"),
+loginbtn = document.querySelector("#loginbtn");
+
+console.log(id, psword, loginbtn);
+
+const login = (e) => {
+  const req = {
+    id: id.value,
+    psword: psword.value
+  };
+  console.log(req);
+
+  // fetch()
+}
+
+loginbtn.addEventListener("click", login);
+
